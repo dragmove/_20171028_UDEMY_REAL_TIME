08dfc349f3ca088760c55aabae13a4e28dd12daa
김현석 <dragmove@naver.com>
2017-11-23 18:00:17 +0900

    Task: working 35. Logging In with the Chat Form

--- src/client/components/chat/form.js
+++ src/client/components/chat/form.js
@@ -1,14 +1,42 @@
 import $ from 'jquery';
+import {Observable} from 'rxjs';
+
 import {ElementComponent} from '../../lib/component';
 
 export class ChatFormComponent extends ElementComponent {
-  constructor() {
+  constructor(usersStore) {
     super('div');
+
+    this._users = usersStore;
 
     this.$element.addClass('chat-form');
   }
 
   _onAttach() {
     this._$input = $(`<input type="text" class="chat-input" />`).appendTo(this.$element);
+
+    this._users.currentUser$.compSubscribe(this, user => {
+      this._$input.attr('placeholder', user.isLoggedIn ? '' : 'Enter a username');
+    });
+
+    Observable.fromEvent(this._$input, 'keydown')
+      //get value
+      .filter(e => e.keyCode === 13) // enter key
+      .do(e => e.preventDefault())
+      .map(e => e.target.value.trim())
+      .filter(e => e.length)
+
+      // login or send message
+      .withLatestFrom(this._users.currentUser$)
+      .flatMap(([value, user]) => {
+        return (user.isLoggedIn) ? this._sendMessage$(value) : this._login$(value);
+      })
+
+      // display message
+      .compSubscribe(this, response => {
+
+
+
+      });
   }
 }
